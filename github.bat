75cb2d15a27f2092f474cb146afb83c16a8d92a7
Atin Gupta <atingupta2005@gmail.com>
2025-10-06 07:47:21 +0530

    -

--- github.bat
+++ github.bat
@@ -2,6 +2,6 @@ del .gitignore
 copy .gitignore-github .gitignore
 git add .
 git commit -m "-"
-git pull github-origin main
-git push github-origin main
+git pull origin main
+git push origin main
 
